2c35091cbb9a904d198cabeca24abe935be0d4e6
Issa <chrisissifou@gmail.com>
2021-10-02 13:21:49 +0200

    modification

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Ready!");
+    
+    const btn = document.getElementById('btn')
+    console.log(btn)
+    const paragraphe = document.getElementById('paragraphe')
+
+    if (btn) {
+        btn.addEventListener('click', function() {
+            console.log('eeeeeeeeee')
+            paragraphe.style.display = "block";
+        })
+    }
+    
+
 }, false);
 
 // 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
